2c9680332789e4e33e0241606fd940c22ddaa1c7
psychobunny <psycho.bunny@hotmail.com>
2014-04-19 15:21:25 -0400

    added check for cash mod

--- openfantasy.js
+++ openfantasy.js
@@ -4,9 +4,11 @@ var	fs = require('fs'),
 	async = require('async'),
 	path = require('path'),
 	nconf = require('nconf'),
-	data = require('./data/data.js'),
-	templates = module.parent.require('../public/src/templates.js'),
-	translator = module.parent.require('../public/src/translator.js');
+	winston = require('winston'),
+	data = require('./data/data'),
+	plugins = module.parent.require('./plugins'),
+	templates = module.parent.require('../public/src/templates'),
+	translator = module.parent.require('../public/src/translator');
 
 
 
@@ -20,13 +22,15 @@ var constants = Object.freeze({
 	"image_path": nconf.get('url') + "/plugins/nodebb-plugin-openfantasy/images/"
 });
 
-OF.init = function(app, middleware, controllers) {
-	//todo: check here if cash mod is activated.
+function initialize(app, middleware, controllers) {
 	require('./lib/nodebb');
 	
 	require('./lib/middleware')(app, middleware);
 	require('./lib/controllers')(controllers);
 	require('./lib/routes/main')(app, middleware, controllers);
+
+	setupTranslations(app);
+	data = normalizeImagePaths(data);
 
 	require('./lib/alignments').init(data.alignments);
 	require('./lib/classes').init(data.classes);
@@ -36,36 +40,43 @@ OF.init = function(app, middleware, controllers) {
 	require('./lib/monsters').init(data.monsters);
 	require('./lib/races').init(data.races);
 	require('./lib/stores').init(data.stores);
+}
 
-	function setupTranslations() {
-		// todo: need to add all translations in directory
-		var lang = 'en_GB';
-		translator.addTranslation(lang, 'of', require('./static/language/' + lang + '/openfantasy.json'));
+function setupTranslations(app) {
+	// todo: need to add all translations in directory
+	var lang = 'en_GB';
+	translator.addTranslation(lang, 'of', require('./static/language/' + lang + '/openfantasy.json'));
 
-		app.get('/language/' + lang + '/of.json', function(req, res, next) {
-			res.send(200, require('./static/language/' + lang + '/openfantasy.json'));
-		});
-	}
+	app.get('/language/' + lang + '/of.json', function(req, res, next) {
+		res.send(200, require('./static/language/' + lang + '/openfantasy.json'));
+	});
+}
 
-	function normalizeImagePaths(data) {
-		for (var d in data) {
-			if (data.hasOwnProperty(d)) {
-				if (data[d].constructor === Array || data[d] instanceof Object) {
-					data[d] = normalizeImagePaths(data[d]);
-				} else {
-					var extension = data[d].substr(data[d].length - 4).toLowerCase();
-					if (extension === '.png' || extension === '.jpg' || extension === '.jpeg' || extension === '.gif') {
-						data[d] = constants.image_path + data[d];
-					}
+function normalizeImagePaths(data) {
+	for (var d in data) {
+		if (data.hasOwnProperty(d)) {
+			if (data[d].constructor === Array || data[d] instanceof Object) {
+				data[d] = normalizeImagePaths(data[d]);
+			} else {
+				var extension = data[d].substr(data[d].length - 4).toLowerCase();
+				if (extension === '.png' || extension === '.jpg' || extension === '.jpeg' || extension === '.gif') {
+					data[d] = constants.image_path + data[d];
 				}
 			}
 		}
-
-		return data;
 	}
-	
-	setupTranslations();
-	OF.data = normalizeImagePaths(OF.data);
+
+	return data;
+}
+
+OF.init = function(app, middleware, controllers) {
+	plugins.isActive('nodebb-plugin-cash', function(err, isActive) {
+		if (!isActive) {
+			return winston.error('[openfantasy] Cash MOD not installed, OpenFantasy has been deactivated');
+		}
+
+		initialize(app, middleware, controllers);
+	});
 };
 
 OF.addNavigation = function(custom_header, callback) {
