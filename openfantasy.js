eb1a54ad7e1f33fa4875f2671aa2ed3d06e37df0
psychobunny <psycho.bunny@hotmail.com>
2015-04-03 11:30:35 -0400

    new path to translator

--- openfantasy.js
+++ openfantasy.js
@@ -9,7 +9,7 @@ var	fs = require('fs'),
 	meta = module.parent.require('./meta'),
 	plugins = module.parent.require('./plugins'),
 	templates = module.parent.require('templates.js'),
-	translator = module.parent.require('../public/src/translator');
+	translator = module.parent.require('../public/src/modules/translator');
 
 
 
